70b66715b170229321faea67b5915bc809951485
harry2015x <fonsecaharryfonseca@gmail.com>
2026-06-10 10:50:36 -0500

    se borro un boton

--- app/ganado/page.tsx
+++ app/ganado/page.tsx
@@ -31,10 +31,7 @@ export default function GanadoPage() {
         <h1 className="text-4xl font-bold text-green-700 mb-6">
           Inventario Ganadero
         </h1>
-        <button
-  className="bg-green-700 text-white px-4 py-2 rounded mb-6 hover:bg-green-800"
->
-  ➕ Registrar Animal
+        
   <button
   onClick={() => setMostrarFormulario(!mostrarFormulario)}
   className="bg-blue-600 text-white px-4 py-2 rounded mb-6 ml-4"
