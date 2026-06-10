7036135cc9a65e0f5b59f651c5b4282acd49e6b7
harry2015x <fonsecaharryfonseca@gmail.com>
2026-06-10 17:03:28 -0500

    pesaje

---
+++ pesajes/page.tsx
@@ -0,0 +1,9 @@
+export default function PesajesPage() {
+    return (
+      <main className="p-10">
+        <h1 className="text-4xl font-bold text-green-700">
+          Control de Pesajes
+        </h1>
+      </main>
+    );
+  }
