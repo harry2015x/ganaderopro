722f0cf316d9d7e9b00598946609a4cd4ecf81ca
harry2015x <fonsecaharryfonseca@gmail.com>
2026-06-24 16:11:49 -0500

    notificaciones

---
+++ app/notificaciones/page.tsx
@@ -0,0 +1,13 @@
+export default function NotificacionesPage() {
+    return (
+      <div className="p-8">
+        <h1 className="text-3xl font-bold text-green-700">
+          Notificaciones
+        </h1>
+  
+        <p className="text-gray-500 mt-3">
+          Próximamente...
+        </p>
+      </div>
+    );
+  }
